5303459d4bb45b86a54a3ef50404106fa9946ef8
larissadalimar <larissadalimar@dcc.ufrj.br>
2023-01-09 10:54:58 -0300

    fix: auth in timeline

--- src/components/PublishedCards.js
+++ src/components/PublishedCards.js
@@ -4,13 +4,20 @@ import Card from "./Card";
 
 export default function PublishedCards() {
   const [cards, setCards] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
     setLoading(true);
-    axios.get(`${process.env.REACT_APP_API_BASE_URL}/timeline`)
+    axios.get(`${process.env.REACT_APP_API_BASE_URL}/timeline`,
+    {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
       .then((res) => {
-        setCards(res.data);
+        setCards(res.data.posts);
         setLoading(false);
         console.log(res.data);
       })
@@ -23,8 +30,8 @@ export default function PublishedCards() {
     <>
         {loading?
             <h1>Loading...</h1> :
-            (cards.map((card) => <Card key={card.id} card={card} />))
+            (cards?.map((card) => <Card key={card.id} card={card} />))
         }
-        </>
+    </>
   );
 }

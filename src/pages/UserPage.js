7285282f39ac806bf5cad05bc1a8e6e8e136915c
Paulo-Junio <paulofernandesbraga@gmail.com>
2023-01-13 01:36:51 -0300

    FEAT: follow/unfollow

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,32 +1,77 @@
 import styled from "styled-components"
 import PublishedCards from "../components/PublishedCards"
 import TrendingCards from "../components/TrendingCards"
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import axios from "axios";
-import { baseURL } from "../constants/urls";
+import { API_URL } from "../constants/urls";
 import { titleFont } from "../constants/fonts"
 import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar"
-import { useAuth } from "../providers/auth";
+import { AuthContext } from "../providers/auth";
 
 export default function UserPage() {
-    const { token } = useAuth();
+    const { token } = useContext(AuthContext);;
+    console.log("token:",token)
     const [cards, setCards] = useState([]);
     const [hashtags, setHashtags] = useState([]);
     const [loading, setLoading] = useState(true);
     const [name,setName] = useState("");
+    const [following, setFollowing] = useState('');
+    const[disabled, setDisabled] = useState(false)
     const { id } = useParams();
 
+    
+    const followUser= ()=>{
+        setDisabled(true)
 
-    useEffect(() => {
-        axios.get(`${baseURL}/user/${id}`,{
+        axios.post(`${API_URL}/user/${id}`,{},{headers: { authorization: `Bearer ${token}`}}
+        )
+        .then((res) => {
+            console.log(res.data)
+            setFollowing(true)
+            setDisabled(false)
+            return;
+        })
+        .catch((err) => {
+            console.log(err);
+            alert("Não foi possivel deixar de seguir esse usuário. Tente novamente!")
+            setDisabled(false)
+        
+        })
+    }
+
+    const unfollowUser= ()=>{
+        setDisabled(true)
+        console.log({
+            headers: { Authorization: `Bearer ${token}` },
+        })
+        axios.delete(`${API_URL}/user/${id}`,{
             headers: { Authorization: `Bearer ${token}` },
         })
         .then((res) => {
+            console.log(res.data)
+            setFollowing(false)
+            setDisabled(false)
+            return;
+        })
+        .catch((err) => {
+            console.log(err);
+            alert("Não foi possivel deixar de seguir esse usuário. Tente novamente!")
+            setDisabled(false)
+        
+        })
+    }
+    useEffect(() => {
+        axios.get(`${API_URL}/user/${id}`,{
+            headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {
+            console.log(res.data)
             setName(res.data.posts[0].username);
             setCards(res.data.posts);
             setHashtags(res.data.hashtags);
             setLoading(false);
+            setFollowing(res.data.userFollow)
             return;
         })
         .catch((err) => {
@@ -35,6 +80,7 @@ export default function UserPage() {
         
         })
     }, [token,id]);
+
 
  
     return(
@@ -52,9 +98,11 @@ export default function UserPage() {
         }
         )}
           
-                <TrendingCards hashtags={hashtags}/>
-    
-        
+        <TrendingCards hashtags={hashtags}/>
+        {
+        following? (<Button className="unfollowButton" disabled={disabled} onClick={unfollowUser}>Unfollow</Button>) :
+                        (<Button className="followButton" disabled={disabled} onClick={followUser}>Follow</Button> ) 
+        }
         </UserContainer>
       
         
@@ -69,6 +117,16 @@ const UserContainer = styled.div`
     margin-left: 12%;
     padding-bottom: 20px;
     margin-bottom: 20px;
+
+    .unfollowButton{
+        background-color:#FFFFFF;
+        color:#1877F2;
+    }
+    .followButton{
+        background-color:#1877F2;
+        color:#FFFFFF;
+    }
+
     
 `
 
@@ -90,4 +148,15 @@ const Load = styled.h1`
     font-size: 30px;
     line-height: 30px;
     color: white;
+`
+const Button = styled.button`
+        border-width: 0;
+        border-radius: 5px;
+        height:31px ;
+        width:112px;
+
+        position: fixed;
+        top:150px;
+        left:80%;
+        cursor: pointer;
 `

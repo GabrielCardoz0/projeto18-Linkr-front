96b370d43698020d7c90b009e4244f943404a012
gusgusz <gustavoguimaraescamps@gmail.com>
2023-01-13 14:00:55 -0300

    fix: last commit

--- src/components/TrendingCards.js
+++ src/components/TrendingCards.js
@@ -28,9 +28,9 @@ const Container = styled.div`
     border-radius: 16px;
     width: 301px;
     height: 430px;
-    position: absolute;
-    margin-top: 272px;
-    margin-left: 54%;
+    position: fixed;
+    top: 272px;
+    left: 66.5%;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     h3{
         font-family: Lato;
